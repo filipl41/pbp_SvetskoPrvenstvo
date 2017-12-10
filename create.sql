f085e4948a0b88dd518c8013b9c14e47eb08eff3
filipl41 <filipl41@yahoo.com>
2017-12-10 22:38:20 +0100

    Add files via upload
    Ispravljene greske u EER dijagramu, dodati create i insert

---
+++ create.sql
@@ -0,0 +1,85 @@
+
+drop database if exists SVETSKO_PRVENSTVO;
+create database SVETSKO_PRVENSTVO;
+use SVETSKO_PRVENSTVO;
+
+create table Drzava (
+	id int not null primary key,
+	naziv varchar(45) not null
+);
+
+create table Reprezentacija (
+	pozicijaNaFibaListi int not null,
+	id int not null references Drzava(id),
+	unique(pozicijaNaFibaListi)
+);
+
+create table Igrac (
+	idIgraca int not null primary key,
+	ime varchar(45) not null,
+	prezime varchar(45) not null,
+	datumRodjenja date not null,
+	idDrzava int not null references Drzava(id)
+);
+
+create table Strucni_stab (
+	id int not null primary key,
+	ime varchar(45) not null,
+	prezime varchar(45) not null,
+	datumRodjenja date not null,
+	idDrzava int not null references Drzava(id)
+);
+
+create table Statistika (
+	brojPoena int not null default 0,
+	brojAsistencija int not null default 0,
+	brojBlokada int not null default 0,
+	brojSkokova int not null default 0,
+	brojPromasenihSuteva int not null default 0,
+	brojIzgubljenihLopti int not null default 0,
+	brojFaulova int not null default 0,
+	indeksKorisnosti int as (brojPoena + brojAsistencija + brojBlokada + brojSkokova - brojPromasenihSuteva - brojIzgubljenihLopti - brojFaulova),
+	id int not null references Igrac(idIgraca)
+	);
+	
+create table Sudija (
+	idSudije int not null primary key,
+	ime varchar(45) not null,
+	prezime varchar(45) not null,
+	idDrzava int not null references Drzava(id)
+);
+
+create table Grad (
+	idGrada int not null primary key,
+	naziv varchar(45) not null
+);
+
+create table Grupa (
+	id varchar(45) not null,
+	bodovi int not null,
+	odigranoUtakmica int not null,
+	kosRazlika int not null,
+	idReprezentacija int not null references Reprezentacija(id),
+	idGrada int not null references Grad(idGrada)
+);
+
+create table Utakmica (
+	id int not null primary key,
+	ishod int not null,
+	datum date not null,
+	poeniGost int not null,
+	poeniDomacin int not null,
+	poeniUkupno int not null,
+	idSudije int not null references Sudija(idSudije),
+	domacin int not null references Grupa(idReprezentacija),
+	gost int not null references Grupa(idReprezentacija),
+	faza varchar(45) not null,
+	check(ishod in (1, 2)),
+	check (faza in ("finale", "polufinale", "cetvrtfinale", "osmina","sesnestina" "grupa"))
+
+);
+
+create table Nokaut_faza (
+	idUtakmice int not null references Utakmica(id),
+	idGrada int not null references Grad(idGrada)
+);
---
+++ insert.sql
@@ -0,0 +1,116 @@
+
+insert into Drzava values
+(1, 'Serbia'),
+(2, 'Spain'),
+(3, 'USA'),
+(4, 'Lithuania'),
+(5, 'Turkey'),
+(6, 'Greece'),
+(7, 'Senegal'),
+(8, 'France'),
+(9, 'Slovenia'),
+(10, 'Argentina'),
+(11, 'Brazi'),
+(12, 'China'),
+(13, 'Australia'),
+(14, 'Jamaica'),
+(15, 'Japan'),
+(16, 'Russia'),
+(17, 'Puerto Rico'),
+(18, 'Egypt'),
+(19, 'Finland'),
+(20, 'Italy'),
+(21, 'Mexico'),
+(22, 'Latvia'),
+(23, 'New Zeland'),
+(24, 'Nigeria'),
+(25, 'Croatia'),
+(26, 'South Korea'),
+(27, 'Macedonia'),
+(28, 'Estonia');
+
+
+insert into Reprezentacija values
+(2, 1),
+(3, 2),
+(1, 3),
+(4, 4),
+(12, 5),
+(5, 6),
+(28, 7),
+(20, 8),
+(6, 9),
+(7, 10),
+(10, 11),
+(30, 12),
+(11, 13),
+(40, 14),
+(22, 15),
+(9, 16),
+(23, 17),
+(31, 18),
+(19, 19),
+(13, 20),
+(29, 21),
+(14, 22),
+(37, 23),
+(21, 24);
+
+
+insert into Grad values 
+(11, 'Bejing'),
+(22, 'Guangzhou'),
+(33, 'Shanghai'),
+(44, 'Wuhan');
+
+
+insert into Grupa values
+('A', 0, 0, 0, 1, 11),
+('A', 0, 0, 0, 8, 11),
+('A', 0, 0, 0, 14, 11),
+('A', 0, 0, 0, 20, 11),
+('A', 0, 0, 0, 9, 11),
+('A', 0, 0, 0, 23, 11),
+('B', 0, 0, 0, 2, 22),
+('B', 0, 0, 0, 7, 22),
+('B', 0, 0, 0, 13, 22),
+('B', 0, 0, 0, 24, 22),
+('B', 0, 0, 0, 12, 22),
+('B', 0, 0, 0, 22, 22),
+('C', 0, 0, 0, 3, 33),
+('C', 0, 0, 0, 6, 33),
+('C', 0, 0, 0, 11, 33),
+('C', 0, 0, 0, 21, 33),
+('C', 0, 0, 0, 15, 33),
+('C', 0, 0, 0, 16, 33),
+('D', 0, 0, 0, 4, 44),
+('D', 0, 0, 0, 5, 44),
+('D', 0, 0, 0, 10, 44),
+('D', 0, 0, 0, 19, 44),
+('D', 0, 0, 0, 17, 44),
+('D', 0, 0, 0, 18, 44);
+
+
+insert into Sudija values
+(1, 'Ilija', 'Belosevic', 1),
+(2, 'Milija', 'Vojinovic', 1),
+(3, 'Luigi', 'Lamonica', 20);
+
+
+insert into Strucni_stab values
+(1, 'Aleksandar', 'Djordjevic', DATE '1967-8-12', 1),
+(2, 'Nebojsa', 'Ilic', DATE '1970-4-7', 1),
+(3, 'Gregg', 'Popovich', DATE '1955-5-1', 3);
+
+
+insert into Igrac values 
+(1, 'Milos', 'Teodosic', DATE '1985-4-4', 1),
+(2, 'Pau', 'Gasol', DATE '1980-2-7', 2),
+(3, 'LeBron', 'James', DATE '1983-4-8', 3),
+(4, 'Mingaugas', 'Kuzminsas', DATE '1988-12-11', 4),
+(5, 'Omer', 'Asik', DATE '1990-3-8', 5),
+(6, 'Jorgos', 'Printezis', DATE '1987-7-7', 6),
+(7, 'Boris', 'Ndour', DATE '1991-11-8', 7),
+(8, 'Mathias', 'Lessort', DATE '1995-7-4', 8),
+(9, 'Luka', 'Doncic', DATE '1998-1-12', 9),
+(10, 'Luis', 'Scola', DATE '1980-7-7', 10);
